d29bd5ec3854518de3eef59512b2dada073773ff
AekdyCoin <chenh1989@gmail.com>
2014-01-17 14:39:37 -0800

    build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imbiss::Application.config.secret_key_base = '057445e349b5e9902b29cc69a006ccc6879daf20e82a07ca126b59e26bedcaaddaa1bbffdbbb774224216622c31ed2cb8818820ff945160fdaa856486c0255b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imbiss::Application.config.session_store :cookie_store, key: '_imbiss_session'
