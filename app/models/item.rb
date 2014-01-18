b22fd4880129c8b344172f47d1898da84900057b
AekdyCoin <chenh1989@gmail.com>
2014-01-17 18:22:29 -0800

    add mongo const & add Item details

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,21 @@
+
 class Item
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  #index({ :created_at => 1}, expire_after_seconds: 20.seconds);
+
+  field :id , type: Integer
+
+  field :img_src, type: String
+  field :category, type: String
+  field :name, type: String
+  field :description, type: String
+  field :price, type: Float
+  field :quantity, type: Float
+  field :unit, type: String
+
+
+  index({:sid => Mongo::ASCENDING}, {unique: true})
 
 end
---
+++ config/initializers/mongo_constants.rb
@@ -0,0 +1,5 @@
+module Mongo
+  ASCENDING = +1
+  DESCENDING = -1
+  GEO2D = "2d"
+end
