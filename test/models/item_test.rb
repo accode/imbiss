87e973e26b4aba22a7df63a02f5c173ff0ba114a
AekdyCoin <chenh1989@gmail.com>
2014-01-24 12:13:42 -0800

    try test

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "item test" do
+    it = Item.new(:name => "name", :description => "des" , :price => 3.21,:category => "meat", :img_src => "src_url" , :quantity => 1.0, :unit => "$")
+    it.save
+    fit = Item.find( it.id )
+    assert_equal it, fit
+    printf "DONE #{fit.inspect} count = #{Item.count}\n"
+  end
 end
