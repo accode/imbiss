6f85ee7f7833125321e1de181f843c5f100991b7
AekdyCoin <chenh1989@gmail.com>
2014-01-27 16:25:35 -0800

    add item test unit

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
   test "item test" do
+    Item.delete_all;
     it = Item.new(:name => "name", :description => "des" , :price => 3.21,:category => "meat", :img_src => "src_url" , :quantity => 1.0, :unit => "$")
     it.save
     fit = Item.find( it.id )
     assert_equal it, fit
     printf "DONE #{fit.inspect} count = #{Item.count}\n"
+    it.destroy;
   end
 end
